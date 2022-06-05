d589b382eeaa08a773477dff51dd005856eec51e
rahul-6472 <rahulpandey0663@gmail.com>
2022-06-05 21:04:52 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <div> 
+          <h1>Mobile Operating Systems</h1>
+          <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+          </ul>
+     </div>
+
+     <div className="two"> 
+          <h1>Mobile Manufacturers</h1>
+          <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+          </ul>
+     </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
